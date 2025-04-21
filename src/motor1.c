f3d1514598e75a582d9eacafa853f3e9203347ab
Morten Idemar Andersen Tenmann <mortenidemar@gmail.com>
2025-04-21 10:04:03 +0200

    Lagt til motor driver

---
+++ src/motor1.c
@@ -0,0 +1,4 @@
+double motor1_juster_motor(double target_styrke) {
+    // I den virkelige verden ville driver motoren kodes her
+    return target_styrke;
+}
---
+++ src/motor3.c
@@ -0,0 +1,4 @@
+double motor3_juster_motor(double target_styrke) {
+    // I den virkelige verden ville driver motoren kodes her
+    return target_styrke;
+}
