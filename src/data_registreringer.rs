5809ef87ed723ca8d78c62c8abc22f89dce0656d
Morten Idemar Andersen Tenmann <mortenidemar@gmail.com>
2025-04-23 06:58:32 +0200

    La inn funksjon i data_registreringer.rs

--- src/data_registreringer.rs
+++ src/data_registreringer.rs
@@ -29,3 +29,18 @@ struct enkelDataRegistrerer<W: Write> {
     registrer_styrke: Vec<u64>,
 }
 
+pub fn nyEnkelDataRegistrerer(esp: Box<Bygning>) -> Box<DataRegistreringer> {
+    let termsize = termion::treminal_size().ok();
+    Box::new(enkelDataRegistrerer {
+        esp: esp.clone(),
+        termbredde: termsize.map(|(w, _)| w -2).expect("termbredde") as u64,
+        termhøyde: termsize.map(|(h, _)| h -2).expect("termhøyde") as u64,
+        stdout: io::stdout().into_raw_mode().unwrap(),
+        logg: File::create("simulator.log").expect("logg fil"),
+        registrer_lokasjon: Vec::new(),
+        registrer_hastighet: Vec::new(),
+        registrer_akselerasjon: Vec::new(),
+        registrer_styrke: Vec::new(),
+    })
+}
+
